d7226fbe4d74f954b4b5096d22e8c46d685f2b9d
baaicha <aichababa098@gmail.com>
2025-04-19 17:06:26 +0000

    mise a jour

--- tests/test.sh
+++ tests/test.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-echo " Lancement du test d’accessibilité de l'application..."
+# URL de test (à modifier selon ton NodePort ou LoadBalancer)
+URL="http://localhost:8000"
 
-# Redirige les ports du service (modifie le nom du service si nécessaire)
-kubectl port-forward service/book-app-service 8000:80 &
-PORT_FORWARD_PID=$!
-
-sleep 5
-
-# Test via curl (modifie la chaîne recherchée selon ta page d'accueil)
-curl -s http://localhost:8000 | grep -q "Liste des Livres" && echo "Test OK" || (echo " Test échoué" && kill $PORT_FORWARD_PID && exit 1)
-
-# Stopper le port-forward proprement
-kill $PORT_FORWARD_PID
+# Test de contenu de la page
+curl -s $URL | grep -q "Liste des Livres" && echo " Test OK" || echo " Test Échoué"
